9f30b0a90a1a8ffb9589df593660509df0526b96
ejprice54321 <emma.price@students.olin.edu>
2017-01-31 22:40:36 -0500

    adding thinkos 3 to exercise 2.5

--- homework/hw01/hello.c
+++ homework/hw01/hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-  int x=5;
-  int y = x+1;
+  /* optimization gets rid of variables not in use and
+does some minor calculations because it can get rid of
+x when it's only use is in y
+  */
+  int x=5; //when x is printed it shows up
+  int y = x + 1; // when y is printed it shows up
+  // when x is in y it also disappears
   printf("%i\n", y);
 }
