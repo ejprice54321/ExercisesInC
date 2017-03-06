1528155308e912541b59082f0235f6e508e5581a
ejprice54321 <emma.price@students.olin.edu>
2017-03-05 22:50:17 -0500

    adding hw5

---
+++ homework/hw05/find_track_oo.c
@@ -0,0 +1,108 @@
+/* Example code for Exercises in C.
+
+Modified version of an example from Chapter 2.5 of Head First C.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+#define NUM_TRACKS 5
+
+char tracks[][80] = {
+    "So What",
+    "Freddie Freeloader",
+    "Blue in Green",
+    "All Blues",
+    "Flamenco Sketches"
+};
+
+
+typedef struct {
+    regex_t inner_struct[1];
+} Regex;
+
+
+/* Returns a new Regex that matches the given pattern.
+ *
+ * pattern: string regex
+ * flags: flags passed to regcomp
+ * returns: new Regex
+ */
+Regex *make_regex(char *pattern, int flags) {
+  Regex *regex;
+
+  regex_t regex_add;
+
+  regex->inner_struct[1] = regex_add;
+
+  int ret = regcomp(&regex_add, pattern, flags);
+
+  if (ret) {
+      fprintf(stderr, "Could not compile regex\n");
+      exit(1);
+  } else{
+      return regex;
+  }
+}
+
+/* Checks whether a regex matches a string.
+ *
+ * regex: Regex
+ * s: string
+ * returns: 1 if there's a match, 0 otherwise
+ */
+int regex_match(Regex *regex, char *s) {
+
+  char msgbuf[100];
+
+  int ret = regexec(*regex, s, 0, NULL, 0);
+  if (!ret){
+    return 1;
+  }else if (ret == REG_NOMATCH) {
+    continue;
+  } else {
+    regerror(ret, &regex, msgbuf, sizeof(msgbuf));
+    fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+    exit(1);
+  }
+}
+
+/* Frees a Regex.
+ *
+ * regex: Regex
+ */
+void regex_free(Regex *regex) {
+  regfree(&regex);
+}
+
+
+/* Finds all tracks that match the given pattern.
+ *
+ * Prints track number and title.
+ */
+void find_track_regex(char pattern[])
+{
+    int i;
+
+    Regex *regex = make_regex(pattern, REG_EXTENDED | REG_NOSUB);
+
+    for (i=0; i<NUM_TRACKS; i++) {
+    	if (regex_match(regex, tracks[i])) {
+    	    printf("Track %i: '%s'\n", i, tracks[i]);
+    	}
+    }
+
+    regex_free(regex);
+}
+
+
+int main (int argc, char *argv[])
+{
+    char *pattern = "F.*F.*";
+    find_track_regex(pattern);
+
+    return 0;
+}
